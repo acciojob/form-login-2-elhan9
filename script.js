4188015b637fa60a96bab448ef53e5adaafbde4c
elhan9 <118253118+elhan9@users.noreply.github.com>
2023-11-13 02:20:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let firstName = document.querySelector("#firstName");
+let lastName = document.querySelector("#lastName")
+let email = document.querySelector("#emailid")
+let phone = document.querySelector("#Phone")
+let form = document.querySelector("#form")
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    let result = [];
+    result.push(firstName.value, lastName.value, email.value, phone.value);
+    alert(result.join('\n'));
+})
+
